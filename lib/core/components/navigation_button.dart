369947a937b1a92b02e9a7af9b367b1b7b5e189f
Joseph Chingalo <profschinfalo@gmail.com>
2020-08-26 09:24:02 +0300

    Update UI for menu

--- lib/core/components/navigation_button.dart
+++ lib/core/components/navigation_button.dart
@@ -3,10 +3,19 @@ import 'package:provider/provider.dart';
 import 'package:todo_app/app-state/page_model.dart';
 import 'package:todo_app/core/utils/util_helpers.dart';
 
+class MenuItem {
+  String title;
+  IconData icon;
+  MenuItem({this.icon, this.title});
+}
+
 class NavigationButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    List<String> menus = ['Home', 'Todo'];
+    List<MenuItem> menus = [
+      MenuItem(title: 'Home', icon: Icons.home),
+      MenuItem(title: 'Todo', icon: Icons.list),
+    ];
     final pageModel = Provider.of<PageModel>(context, listen: false);
     void setCurrentPageTab(int index) {
       pageModel.activateTable(index);
@@ -20,27 +29,39 @@ class NavigationButton extends StatelessWidget {
           int currentIndex = pageState.currentIndex;
           return Row(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: menus.map((String menuItem) {
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: menus.map((MenuItem menuItem) {
               int index = menus.indexOf(menuItem);
               return GestureDetector(
                 onTap: () {
                   return setCurrentPageTab(index);
                 },
                 child: Container(
-                    padding: EdgeInsets.symmetric(vertical: 15, horizontal: 10),
-                    decoration: BoxDecoration(
-                        color: currentIndex == index
-                            ? ThemeData.dark().accentColor
-                            : Colors.transparent,
-                        borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(15),
-                            topRight: Radius.circular(15))),
-                    child: Text(menuItem,
-                        style: UtilHelpers.getFontStyles(
-                            19,
-                            index == currentIndex
-                                ? ThemeData.dark().primaryColor
-                                : Colors.white))),
+                  margin: EdgeInsets.only(top: 9),
+                  padding: EdgeInsets.symmetric(vertical: 15, horizontal: 12),
+                  decoration: BoxDecoration(
+                      color: currentIndex == index
+                          ? ThemeData.dark().accentColor
+                          : Colors.transparent,
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(12),
+                          topRight: Radius.circular(12))),
+                  child: SizedBox(
+                      height: 50,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        mainAxisSize: MainAxisSize.max,
+                        children: [
+                          Icon(menuItem.icon),
+                          Text(menuItem.title,
+                              style: UtilHelpers.getFontStyles(
+                                  19,
+                                  index == currentIndex
+                                      ? ThemeData.dark().primaryColor
+                                      : Colors.white))
+                        ],
+                      )),
+                ),
               );
             }).toList(),
           );
