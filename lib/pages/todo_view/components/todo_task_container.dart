b7b71ed6eb2d805a3cb538987c5401e8d9ccbd89
Joseph Chingalo <profschinfalo@gmail.com>
2020-08-27 09:30:56 +0300

    Theme controller on line seperator for todo tasks

--- lib/pages/todo_view/components/todo_task_container.dart
+++ lib/pages/todo_view/components/todo_task_container.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todo_app/app-state/app_theme_state.dart';
+import 'package:todo_app/core/providers/preference_provider.dart';
 import 'package:todo_app/core/utils/util_helpers.dart';
 import 'package:todo_app/models/todo.dart';
 
@@ -8,30 +11,36 @@ class TodoTaskContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.symmetric(horizontal: 10),
-      decoration: BoxDecoration(
-          border:
-              Border(bottom: BorderSide(color: ThemeData.dark().accentColor))),
-      child: ListTile(
-        trailing: Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            GestureDetector(
-              onTap: () => {},
-              child: Icon(Icons.edit),
-            ),
-            GestureDetector(
-              onTap: () => {},
-              child: Icon(Icons.delete),
-            )
-          ],
+    return Consumer<AppThemeState>(builder: (context, appThemeState, child) {
+      String theme = appThemeState.currentTheme;
+      return Container(
+        margin: EdgeInsets.symmetric(horizontal: 10),
+        decoration: BoxDecoration(
+            border: Border(
+                bottom: BorderSide(
+                    color: theme == PreferenceProvider.darkTheme
+                        ? ThemeData.dark().accentColor
+                        : ThemeData().accentColor))),
+        child: ListTile(
+          trailing: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              GestureDetector(
+                onTap: () => {},
+                child: Icon(Icons.edit),
+              ),
+              GestureDetector(
+                onTap: () => {},
+                child: Icon(Icons.delete),
+              )
+            ],
+          ),
+          title:
+              Text('Summary Chart', style: UtilHelpers.getFontStyles(18, null)),
+          subtitle:
+              Text('Chart to come', style: UtilHelpers.getFontStyles(15, null)),
         ),
-        title:
-            Text('Summary Chart', style: UtilHelpers.getFontStyles(18, null)),
-        subtitle:
-            Text('Chart to come', style: UtilHelpers.getFontStyles(15, null)),
-      ),
-    );
+      );
+    });
   }
 }
