c4c36d0bbaf24b8f3621c489cb7d10a7e1785c2d
Joseph Chingalo <profschingalo@gmail.com>
2020-11-06 11:31:40 +0200

    feat: initiate db provider for user module

---
+++ lib/core/offline-db/user_group_member_provider.dart
@@ -0,0 +1,5 @@
+import 'db_provider.dart';
+
+class UserGroupMemberProvider extends DbProvider {
+  final String table = 'user_group_member';
+}
---
+++ lib/core/offline-db/user_group_provider.dart
@@ -0,0 +1,5 @@
+import 'db_provider.dart';
+
+class UserGroupProvider extends DbProvider {
+  final String table = 'user_group';
+}
