4a7fc381c6a33ed46dbcae92f9e6c637bf2c26d5
Joseph Chingalo <profschingalo@gmail.com>
2020-12-20 14:18:36 +0300

    refactor: aBout page styling

--- lib/modules/about/about.dart
+++ lib/modules/about/about.dart
@@ -12,97 +12,102 @@ class About extends StatelessWidget {
     Size size = MediaQuery.of(context).size;
     return Container(
       child: SafeArea(
-          child: Scaffold(
-        appBar: AppBar(
-          elevation: 0.0,
-          title: Text(
-            'About',
-            style: TextStyle().copyWith(
-              fontSize: 14.0,
+        child: Scaffold(
+          appBar: AppBar(
+            elevation: 0.0,
+            title: Text(
+              'About',
+              style: TextStyle().copyWith(
+                fontSize: 14.0,
+              ),
             ),
+            actions: [
+              AppPopUpMenu(
+                currentPage: 'about',
+              )
+            ],
           ),
-          actions: [
-            AppPopUpMenu(
-              currentPage: 'about',
-            )
-          ],
-        ),
-        body: Consumer<AppInfoState>(
-          builder: (context, appInfoState, child) {
-            return Container(
-              padding: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
-              child: SingleChildScrollView(
-                child: Center(
-                  child: Card(
-                    child: Container(
-                      margin: EdgeInsets.symmetric(vertical: 20),
-                      child: Column(
-                        children: [
-                          Container(
-                            height: size.height * 0.2,
-                            child: SvgPicture.asset(
-                              'assets/logos/todo-logo.svg',
-                              fit: BoxFit.contain,
+          body: Consumer<AppInfoState>(
+            builder: (context, appInfoState, child) {
+              return Container(
+                padding: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+                child: SingleChildScrollView(
+                  child: Center(
+                    child: Card(
+                      child: Container(
+                        margin: EdgeInsets.symmetric(vertical: 20),
+                        child: Column(
+                          children: [
+                            Container(
+                              height: size.height * 0.2,
+                              child: SvgPicture.asset(
+                                'assets/logos/todo-logo.svg',
+                                fit: BoxFit.contain,
+                              ),
                             ),
-                          ),
-                          Container(
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceAround,
-                              children: [
-                                Text(
-                                  'App Name : ${appInfoState.currentAppName}',
-                                  style: TextStyle().copyWith(
-                                    fontSize: 12.0,
+                            Container(
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceAround,
+                                children: [
+                                  Text(
+                                    'App Name : ${appInfoState.currentAppName}',
+                                    style: TextStyle().copyWith(
+                                      fontSize: 12.0,
+                                    ),
                                   ),
-                                ),
-                              ],
+                                ],
+                              ),
                             ),
-                          ),
-                          Container(
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceAround,
-                              children: [
-                                Text(
-                                    'App Version : ${appInfoState.currentAppVersion}',
-                                    style: TextStyle().copyWith(
-                                      fontSize: 12.0,
-                                    )),
-                              ],
+                            Container(
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceAround,
+                                children: [
+                                  Text(
+                                      'App Version : ${appInfoState.currentAppVersion}',
+                                      style: TextStyle().copyWith(
+                                        fontSize: 12.0,
+                                      )),
+                                ],
+                              ),
                             ),
-                          ),
-                          Container(
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceAround,
-                              children: [
-                                Text('App Id : ${appInfoState.currentAppId}',
-                                    style: TextStyle().copyWith(
-                                      fontSize: 12.0,
-                                    )),
-                              ],
+                            Container(
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceAround,
+                                children: [
+                                  Text('App Id : ${appInfoState.currentAppId}',
+                                      style: TextStyle().copyWith(
+                                        fontSize: 12.0,
+                                      )),
+                                ],
+                              ),
                             ),
-                          ),
-                          Container(
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceAround,
-                              children: [
-                                Text(
-                                    'Device version : ${appInfoState.currentPlatformVersion}',
-                                    style: TextStyle().copyWith(
-                                      fontSize: 12.0,
-                                    )),
-                              ],
+                            Container(
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceAround,
+                                children: [
+                                  Text(
+                                      'Device version : ${appInfoState.currentPlatformVersion}',
+                                      style: TextStyle().copyWith(
+                                        fontSize: 12.0,
+                                      )),
+                                ],
+                              ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
-            );
-          },
+              );
+            },
+          ),
         ),
-      )),
+      ),
     );
   }
 }
