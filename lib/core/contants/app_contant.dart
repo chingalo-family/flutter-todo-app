2ed45d60aaf677deb0bcb99098e2d71b8f73583e
Joseph Chingalo <profschingalo@gmail.com>
2020-12-20 14:22:50 +0300

    refactor: app contants

---
+++ lib/core/contants/app_contant.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppContant {
+  static final int appBarHeight = 70;
+  static final Color darkThemeColor = Color(0XFF212121);
+  static final Color lightThemeColor = Color(0XFF0096FB);
+}
