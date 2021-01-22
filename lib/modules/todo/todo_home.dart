7a1b3b41217e597e54bbd1864ade42b294a2d11b
Joseph Chingalo <profschingalo@gmail.com>
2021-01-22 12:34:10 +0300

    feat: initiate home page for todo

--- lib/modules/todo/todo_home.dart
+++ lib/modules/todo/todo_home.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/core/components/app_bar_container.dart';
+import 'package:todo_app/core/components/app_drawer_container.dart';
+import 'package:todo_app/core/contants/app_contant.dart';
 
 class TodoHome extends StatefulWidget {
   TodoHome({Key key}) : super(key: key);
@@ -10,9 +13,19 @@ class TodoHome extends StatefulWidget {
 class _TodoHomeState extends State<TodoHome> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text('Todo Form'),
+    return SafeArea(
+      child: Scaffold(
+        drawer: AppDrawerContainer(),
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(AppContant.appBarHeight),
+          child: AppBarContainer(
+            title: 'Todo List',
+            isAboutPage: false,
+            isAddTodoVisible: true,
+            isViewChartVisible: true,
+          ),
+        ),
+        body: Container(child: Text('form')),
       ),
     );
   }
