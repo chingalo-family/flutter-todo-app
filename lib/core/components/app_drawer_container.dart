d03e91d3fc9b274ba26497d382b35c1e47243e98
Joseph Chingalo <profschingalo@gmail.com>
2021-01-22 16:06:51 +0300

    feat: including user state on app drawer

--- lib/core/components/app_drawer_container.dart
+++ lib/core/components/app_drawer_container.dart
@@ -1,57 +1,97 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:todo_app/app_state/app_theme_state.dart';
+import 'package:todo_app/app_state/user_state.dart';
 import 'package:todo_app/core/contants/app_contant.dart';
 import 'package:todo_app/core/services/theme_service.dart';
+import 'package:todo_app/models/user.dart';
 
 class AppDrawerContainer extends StatelessWidget {
   const AppDrawerContainer({
     Key key,
   }) : super(key: key);
 
+  onSignInOrSignOut(BuildContext context, User currentUser) {
+    print(currentUser);
+    Navigator.pop(context);
+  }
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Consumer<AppThemeState>(
-      builder: (context, appThemeState, child) {
-        String currentTheme = appThemeState.currentTheme;
-        return Container(
-          width: size.width * 0.8,
-          height: size.height,
-          decoration: BoxDecoration(
-            color: currentTheme == ThemeServices.darkTheme
-                ? AppContant.darkThemeColor
-                : AppContant.lightThemeColor,
-          ),
-          child: Container(
-            child: Column(
-              children: [
-                Container(
-                  height: size.height * 0.2,
-                  child: Center(
-                    child: Text(
-                      'JC',
-                      style: TextStyle().copyWith(color: Colors.white),
-                    ),
+    return Container(
+      alignment: Alignment.topLeft,
+      child: Consumer<AppThemeState>(
+        builder: (context, appThemeState, child) {
+          String currentTheme = appThemeState.currentTheme;
+          return Consumer<UserState>(
+            builder: (context, userState, child) {
+              String usernameIcon = userState.usernameIcon;
+              User currentUser = userState.currrentUser;
+              return Container(
+                width: size.width * 0.8,
+                height: size.height,
+                decoration: BoxDecoration(
+                  color: currentTheme == ThemeServices.darkTheme
+                      ? AppContant.darkThemeColor
+                      : AppContant.lightThemeColor,
+                ),
+                child: Container(
+                  margin: EdgeInsets.symmetric(horizontal: 20.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(
+                          top: size.height * 0.05,
+                          bottom: size.height * 0.025,
+                        ),
+                        height: size.height * 0.1,
+                        child: Visibility(
+                          visible: currentUser.isLogin,
+                          child: Center(
+                            child: CircleAvatar(
+                              radius: size.height * 0.05,
+                              child: Text(
+                                usernameIcon,
+                                style: TextStyle().copyWith(
+                                  color: Colors.white,
+                                  fontSize: 20.0,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        height: size.height * 0.65,
+                        child: Visibility(
+                          visible: currentUser.isLogin,
+                          child: Container(
+                            child: Text(''),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        child: Center(
+                          child: FlatButton(
+                            onPressed: () =>
+                                onSignInOrSignOut(context, currentUser),
+                            child: Text(
+                              currentUser.isLogin ? 'Sign Out' : 'Sign In',
+                              style: TextStyle().copyWith(),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-                Container(
-                  child: Text(
-                    'Detail for ',
-                    style: TextStyle().copyWith(color: Colors.white),
-                  ),
-                ),
-                Container(
-                  child: Text(
-                    'Log in and log out btn',
-                    style: TextStyle().copyWith(color: Colors.white),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        );
-      },
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
