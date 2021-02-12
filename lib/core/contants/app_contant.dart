ca5395941dd7721506cdb358f1a77672cfb95fe1
Joseph Chingalo <profschingalo@gmail.com>
2021-02-12 11:51:08 +0300

    chore: more constants for todo filter

--- lib/core/contants/app_contant.dart
+++ lib/core/contants/app_contant.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class AppContant {
   static final double appBarHeight = 80.0;
+  static final double todoListFilterHeight = 55.0;
   static final Color darkThemeColor = Color(0XFF212121);
   static final Color lightThemeColor = Color(0XFF0096FB);
   static final Color darkTextColor = Color(0xFFD5D5D5);
