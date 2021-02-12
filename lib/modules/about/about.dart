052d9a379e05d49d70643d4f7b8108f141267715
Joseph Chingalo <profschingalo@gmail.com>
2021-02-12 11:27:28 +0300

    feat: color theme for about module

--- lib/modules/about/about.dart
+++ lib/modules/about/about.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:provider/provider.dart';
 import 'package:todo_app/app_state/app_info_state.dart';
+import 'package:todo_app/app_state/app_theme_state.dart';
 import 'package:todo_app/core/components/app_bar_container.dart';
 import 'package:todo_app/core/contants/app_contant.dart';
+import 'package:todo_app/core/services/theme_service.dart';
 
 class About extends StatelessWidget {
   const About({Key key}) : super(key: key);
@@ -23,91 +25,106 @@ class About extends StatelessWidget {
               isViewChartVisible: false,
             ),
           ),
-          body: Consumer<AppInfoState>(
-            builder: (context, appInfoState, child) {
-              return Container(
-                padding: EdgeInsets.symmetric(
-                  vertical: 20,
-                  horizontal: 10,
-                ),
-                child: SingleChildScrollView(
-                  child: Center(
-                    child: Card(
-                      child: Container(
-                        margin: EdgeInsets.symmetric(
-                          vertical: 20,
-                        ),
-                        child: Column(
-                          children: [
-                            Container(
-                              height: size.height * 0.2,
-                              child: SvgPicture.asset(
-                                'assets/logos/todo-logo.svg',
-                                fit: BoxFit.contain,
-                              ),
+          body: Consumer<AppThemeState>(
+            builder: (context, appThemeState, child) {
+              String currentTheme = appThemeState.currentTheme;
+              Color textColor = currentTheme == ThemeServices.darkTheme
+                  ? AppContant.darkTextColor
+                  : AppContant.ligthTextColor;
+              return Consumer<AppInfoState>(
+                builder: (context, appInfoState, child) {
+                  return Container(
+                    padding: EdgeInsets.symmetric(
+                      vertical: 20,
+                      horizontal: 10,
+                    ),
+                    child: SingleChildScrollView(
+                      child: Center(
+                        child: Card(
+                          child: Container(
+                            margin: EdgeInsets.symmetric(
+                              vertical: 20,
                             ),
-                            Container(
-                              child: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceAround,
-                                children: [
-                                  Text(
-                                    'App Name : ${appInfoState.currentAppName}',
-                                    style: TextStyle().copyWith(
-                                      fontSize: 12.0,
-                                    ),
+                            child: Column(
+                              children: [
+                                Container(
+                                  height: size.height * 0.2,
+                                  child: SvgPicture.asset(
+                                    'assets/logos/todo-logo.svg',
+                                    fit: BoxFit.contain,
                                   ),
-                                ],
-                              ),
-                            ),
-                            Container(
-                              child: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceAround,
-                                children: [
-                                  Text(
-                                    'App Version : ${appInfoState.currentAppVersion}',
-                                    style: TextStyle().copyWith(
-                                      fontSize: 12.0,
-                                    ),
+                                ),
+                                Container(
+                                  margin: EdgeInsets.only(
+                                    top: 10.0,
                                   ),
-                                ],
-                              ),
-                            ),
-                            Container(
-                              child: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceAround,
-                                children: [
-                                  Text(
-                                    'App Id : ${appInfoState.currentAppId}',
-                                    style: TextStyle().copyWith(
-                                      fontSize: 12.0,
-                                    ),
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceAround,
+                                    children: [
+                                      Text(
+                                        'App Name : ${appInfoState.currentAppName}',
+                                        style: TextStyle().copyWith(
+                                          fontSize: 12.0,
+                                          color: textColor,
+                                        ),
+                                      ),
+                                    ],
                                   ),
-                                ],
-                              ),
-                            ),
-                            Container(
-                              child: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceAround,
-                                children: [
-                                  Text(
-                                    'Device version : ${appInfoState.currentPlatformVersion}',
-                                    style: TextStyle().copyWith(
-                                      fontSize: 12.0,
-                                    ),
+                                ),
+                                Container(
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceAround,
+                                    children: [
+                                      Text(
+                                        'App Version : ${appInfoState.currentAppVersion}',
+                                        style: TextStyle().copyWith(
+                                          fontSize: 12.0,
+                                          color: textColor,
+                                        ),
+                                      ),
+                                    ],
                                   ),
-                                ],
-                              ),
+                                ),
+                                Container(
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceAround,
+                                    children: [
+                                      Text(
+                                        'App Id : ${appInfoState.currentAppId}',
+                                        style: TextStyle().copyWith(
+                                          fontSize: 12.0,
+                                          color: textColor,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                Container(
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceAround,
+                                    children: [
+                                      Text(
+                                        'Device version : ${appInfoState.currentPlatformVersion}',
+                                        style: TextStyle().copyWith(
+                                          fontSize: 12.0,
+                                          color: textColor,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
                             ),
-                          ],
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                ),
+                  );
+                },
               );
             },
           ),
