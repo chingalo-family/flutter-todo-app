1b9b263b6a911830af1cd235151eb4ffec1c89e6
Joseph Chingalo <profschingalo@gmail.com>
2021-03-02 10:08:06 +0300

    feat: support for edit todo

--- lib/modules/todo/pages/todo_view/todo_view.dart
+++ lib/modules/todo/pages/todo_view/todo_view.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:todo_app/app_state/app_theme_state.dart';
+import 'package:todo_app/app_state/todo_form_state.dart';
 import 'package:todo_app/app_state/todo_state.dart';
 import 'package:todo_app/core/components/app_bar_container.dart';
 import 'package:todo_app/core/contants/app_contant.dart';
+import 'package:todo_app/core/services/theme_service.dart';
+import 'package:todo_app/core/utils/app_util.dart';
+import 'package:todo_app/models/form_section.dart';
 import 'package:todo_app/models/todo.dart';
+import 'package:todo_app/modules/todo/models/todo_form.dart';
+import 'package:todo_app/modules/todo/pages/todo_home/components/todo_form_container.dart';
 
 import 'components/todo_view_container.dart';
 
@@ -14,16 +21,61 @@ class TodoView extends StatelessWidget {
     print("on add todo tasks $currentTodo");
   }
 
-  onEditTodo(BuildContext context, Todo currentTodo) {
-    print("on add todo tasks $currentTodo");
+  onEditTodo(BuildContext context, Todo currentTodo) async {
+    updateTodoFormState(context, currentTodo, !currentTodo.isCompleted);
+    String currentTheme =
+        Provider.of<AppThemeState>(context, listen: false).currentTheme;
+    Color textColor = currentTheme == ThemeServices.darkTheme
+        ? AppContant.darkTextColor
+        : AppContant.ligthTextColor;
+    final List<FormSection> todoFormSections =
+        TodoForm.getFormSections(textColor);
+    Widget modal = TodoFormContainer(
+      todoFormSections: todoFormSections,
+      todoTasks: currentTodo.tasks,
+    );
+    await AppUtil.showPopUpModal(context, modal, false);
   }
 
   onDeleteTodo(BuildContext context, Todo currentTodo) {
-    print("on add todo tasks $currentTodo");
+    print("On deleting");
+    //Navigator.of(context).pop();
   }
 
   onOpenTodoChartSummary(BuildContext context, Todo currentTodo) {
     print("on opening todo chart $currentTodo");
+  }
+
+  updateTodoFormState(
+    BuildContext context,
+    Todo todo,
+    bool isEditableMode,
+  ) {
+    Provider.of<TodoFormState>(context, listen: false).resetFormState();
+    Provider.of<TodoFormState>(context, listen: false)
+        .updateFormEditabilityState(isEditableMode: isEditableMode);
+    if (todo != null) {
+      Provider.of<TodoFormState>(context, listen: false)
+          .setFormFieldState('id', todo.id);
+      Provider.of<TodoFormState>(context, listen: false)
+          .setFormFieldState('title', todo.title);
+      Provider.of<TodoFormState>(context, listen: false)
+          .setFormFieldState('description', todo.description);
+      Provider.of<TodoFormState>(context, listen: false)
+          .setFormFieldState("assignedTo", todo.assignedTo);
+      Provider.of<TodoFormState>(context, listen: false)
+          .setFormFieldState("createdBy", todo.createdBy);
+      Provider.of<TodoFormState>(context, listen: false)
+          .setFormFieldState("createdOn", todo.createdOn);
+      Provider.of<TodoFormState>(context, listen: false)
+          .setFormFieldState("dueDate", todo.dueDate);
+      Provider.of<TodoFormState>(context, listen: false)
+          .setFormFieldState("groupId", todo.groupId);
+      Provider.of<TodoFormState>(context, listen: false)
+          .setFormFieldState("completedOn", todo.completedOn);
+      Provider.of<TodoFormState>(context, listen: false)
+          .setFormFieldState("completedBy", todo.completedBy);
+    }
   }
 
   @override
@@ -43,8 +95,8 @@ class TodoView extends StatelessWidget {
                 isDeleteVisible: true,
                 isEditVisible: true,
                 onAdd: () => onAddTodoTask(context, currentTodo),
-                onEdit: () => onAddTodoTask(context, currentTodo),
-                onDelete: () => onAddTodoTask(context, currentTodo),
+                onEdit: () => onEditTodo(context, currentTodo),
+                onDelete: () => onDeleteTodo(context, currentTodo),
                 onOpenChart: () => onOpenTodoChartSummary(context, currentTodo),
               ),
             ),
