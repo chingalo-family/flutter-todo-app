1e0749485846941254c4810d6f32067b49a969a9
Joseph Chingalo <profschingalo@gmail.com>
2021-03-02 16:33:35 +0300

    fix: checkbox for complete or uncomolete task

--- lib/modules/todo/pages/todo_view/components/todo_task_container.dart
+++ lib/modules/todo/pages/todo_view/components/todo_task_container.dart
@@ -3,6 +3,7 @@ import 'dart:ui';
 import 'package:flutter/cupertino.dart';
 import 'package:provider/provider.dart';
 import 'package:todo_app/app_state/app_theme_state.dart';
+import 'package:todo_app/app_state/todo_state.dart';
 import 'package:todo_app/core/contants/app_contant.dart';
 import 'package:todo_app/core/services/theme_service.dart';
 import 'package:todo_app/core/utils/app_util.dart';
@@ -14,7 +15,7 @@ import 'package:todo_app/modules/todo/models/todo_task_form.dart';
 import 'package:todo_app/modules/todo/pages/todo_view/components/todo_task_card.dart';
 import 'package:todo_app/modules/todo/pages/todo_view/components/todo_task_form_container.dart';
 
-class TodoTaskContainer extends StatelessWidget {
+class TodoTaskContainer extends StatefulWidget {
   const TodoTaskContainer({
     Key key,
     @required this.textColor,
@@ -24,6 +25,11 @@ class TodoTaskContainer extends StatelessWidget {
   final Color textColor;
   final Todo currentTodo;
 
+  @override
+  _TodoTaskContainerState createState() => _TodoTaskContainerState();
+}
+
+class _TodoTaskContainerState extends State<TodoTaskContainer> {
   onEditTodoTask(
     BuildContext context,
     TodoTask todoTask,
@@ -56,6 +62,7 @@ class TodoTaskContainer extends StatelessWidget {
     bool isCompleted,
   ) {
     todoTask.isCompleted = isCompleted;
+    Provider.of<TodoState>(context, listen: false).addTodoTask(todoTask);
   }
 
   @override
@@ -71,24 +78,24 @@ class TodoTaskContainer extends StatelessWidget {
               vertical: 10.0,
             ),
             child: Text(
-              "${currentTodo.title}'s list of tasks",
+              "${widget.currentTodo.title}'s list of tasks",
               style: TextStyle().copyWith(
                 fontSize: 18.0,
-                color: textColor,
+                color: widget.textColor,
               ),
             ),
           ),
           Container(
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
-              children: currentTodo.tasks
+              children: widget.currentTodo.tasks
                   .map(
                     (TodoTask todoTask) => Container(
                       margin: EdgeInsets.symmetric(
                         vertical: 5.0,
                       ),
                       child: TodoTaskCard(
-                        textColor: textColor,
+                        textColor: widget.textColor,
                         todoTask: todoTask,
                         onEdit: () => onEditTodoTask(context, todoTask),
                         onDelete: () => onDeleteTodoTask(context, todoTask),
